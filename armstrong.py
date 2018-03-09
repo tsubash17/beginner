edf7a1d929e76884ccf1c1346fbbd2177f5d894d
RAGUPATHI537 <36394314+RAGUPATHI537@users.noreply.github.com>
2018-03-09 17:44:19 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+print("Enter the number")
+number = int(input())
+temp =number
+Sum = 0
+while(temp != 0):
+    rem  = temp % 10 
+    Sum  = Sum + (rem * rem * rem)
+    temp = temp / 10 #
+if(number == Sum):
+    print ("Armstrong Number")
+else:
+    print ("Not an Armstrong Number")
