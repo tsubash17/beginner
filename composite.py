9b9447866730bc9569080eee425714984de2d377
tsubash17 <36472076+tsubash17@users.noreply.github.com>
2018-03-26 12:19:54 +0530

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,8 +1,8 @@
 a=input()
 if a>1:
     for i in range(2,a):
-        (a%i==0)
-        print (a,"it's composite num")
-        break
-    else:
-        print ("prime num",a)
+        if(a%i==0):
+           print (a,"it's composite num")
+           break
+        else:
+           print ("prime num",a)
